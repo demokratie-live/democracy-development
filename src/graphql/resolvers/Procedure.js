7dca88f051596e4468d4b68f188ec35b8d228006
Manuel Ruck <github@manuelruck.de>
2018-02-18 15:31:25 +0100

    fix graphql model structure

--- src/graphql/resolvers/Procedure.js
+++ src/graphql/resolvers/Procedure.js
@@ -1,8 +1,12 @@
 export default {
   Query: {
-    procedures: (parent, { IDs }, { ProcedureModel }) =>
-      ProcedureModel.find({ type: 'Gesetzgebung', procedureId: { $in: IDs } }),
-    allProcedures: (parent, args, { ProcedureModel }) =>
-      ProcedureModel.find({ type: 'Gesetzgebung' }),
+    procedures: (parent, { IDs }, { ProcedureModel }) => {
+      console.log('getProcedures');
+      return ProcedureModel.find({ type: 'Gesetzgebung', procedureId: { $in: IDs } });
+    },
+    allProcedures: async (parent, args, { ProcedureModel }) => {
+      console.log(await ProcedureModel.find({ type: 'Gesetzgebung' }).count());
+      return ProcedureModel.find({ type: 'Gesetzgebung' });
+    },
   },
 };
