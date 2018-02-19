05d12183e7221f7029ac4606e43a48572cc6e409
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2018-02-19 13:22:38 +0100

    removed console

--- src/graphql/resolvers/Procedure.js
+++ src/graphql/resolvers/Procedure.js
@@ -1,9 +1,10 @@
 export default {
   Query: {
-    procedures: (parent, { IDs }, { ProcedureModel }) => {
-      console.log('getProcedures');
-      return ProcedureModel.find({ type: 'Gesetzgebung', procedureId: { $in: IDs } });
-    },
-    allProcedures: async (parent, args, { ProcedureModel }) => ProcedureModel.find({ type: 'Gesetzgebung', period: { $gte: 18 } }),
+    procedures: (parent, { IDs }, { ProcedureModel }) =>
+      // console.log('getProcedures');
+      ProcedureModel.find({ type: 'Gesetzgebung', procedureId: { $in: IDs } }),
+
+    allProcedures: async (parent, args, { ProcedureModel }) =>
+      ProcedureModel.find({ type: 'Gesetzgebung', period: { $gte: 18 } }),
   },
 };
