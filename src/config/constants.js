572c4390676d1f4862bb791050498e049571cf19
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2018-02-21 16:38:45 +0100

    doesnt work otherwise

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 export default {
-  PORT: process.env.PORT ? process.env.PORT : 3100,
-  DB_URL: process.env.DB_URL ? process.env.DB_URL : 'mongodb://localhost/bundestagio',
+  PORT: process.env.PORT,
+  DB_URL: process.env.DB_URL,
   GRAPHIQL_PATH: '/graphiql',
   GRAPHQL_PATH: '/graphql',
 };
