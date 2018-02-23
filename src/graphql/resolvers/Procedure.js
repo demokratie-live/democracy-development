042cfb193902b46883b5cce6125e8b1716365df5
Manuel Ruck <github@manuelruck.de>
2018-02-23 22:25:13 +0100

    add cronjob to import on default server run webhook on import finish resolve #19

--- src/graphql/resolvers/Procedure.js
+++ src/graphql/resolvers/Procedure.js
@@ -1,8 +1,6 @@
 export default {
   Query: {
-    procedures: (parent, { IDs }, { ProcedureModel }) =>
-      // console.log('getProcedures');
-      ProcedureModel.find({ type: 'Gesetzgebung', procedureId: { $in: IDs } }),
+    procedures: (parent, { IDs }, { ProcedureModel }) => ProcedureModel.find({ type: 'Gesetzgebung', procedureId: { $in: IDs } }),
 
     allProcedures: async (parent, args, { ProcedureModel }) =>
       ProcedureModel.find({ type: 'Gesetzgebung', period: { $gte: 18 } }),
