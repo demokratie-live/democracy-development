d8bb93e681171e99955abab7be95855c921b3cd6
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2018-04-13 10:57:12 +0200

    remove unused docker containers & images automatically after installing

--- start-production.sh
+++ start-production.sh
@@ -13,3 +13,4 @@ git status
 echo "### Start docker Process ###"
 echo "# docker-compose up -d --build $@"
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build $@
+docker system prune | y
