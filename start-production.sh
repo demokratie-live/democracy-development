73e30bd89820220b4d758e23e4ccbcb5c8eae75b
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2018-04-15 18:05:42 +0200

    fixed start-production script

--- start-production.sh
+++ start-production.sh
@@ -3,13 +3,15 @@ echo "### Start git process ###"
 
 echo "# git fetch #"
 git fetch
-echo "# git pull #"
-git pull
+# echo "# git pull #"
+# git pull
+echo "# git checkout tag/$@ #"
+git checkout tags/$@
 echo "# git submodule update --init --recursive #"
 git submodule update --init --recursive
 echo "# git status #"
 git status
 
 echo "### Start docker Process ###"
-echo "# docker-compose up -d --build $@"
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build $@
+echo "# docker-compose up -d --build"
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
