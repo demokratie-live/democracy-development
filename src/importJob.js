353f15f42b52a9e1edcd568414cae2759b28bfe8
Manuel Ruck <github@manuelruck.de>
2018-06-08 14:44:00 +0200

    fix eslint errors

--- src/importJob.js
+++ src/importJob.js
@@ -18,11 +18,11 @@ const log = new Log("error", fs.createWriteStream("error-import.log"));
 // require('./config/db');
 
 const scraper = new Scraper();
-let pastScrapeData = null;
-const procedureStatusWhitelist = [
-  "Überwiesen",
-  "Beschlussempfehlung liegt vor"
-];
+let pastScrapeData = null; // eslint-disable-line
+// const procedureStatusWhitelist = [
+//   "Überwiesen",
+//   "Beschlussempfehlung liegt vor"
+// ];
 let cronIsRunning = false;
 let cronStart = null;
 
@@ -122,51 +122,51 @@ const saveProcedure = async ({ procedureData }) => {
   );
 };
 
-const doScrape = ({ data }) => {
-  const parts = data.date.match(/(\d+)/g);
-  const dipDate = new Date(Date.UTC(parts[2], parts[1] - 1, parts[0]));
+// const doScrape = ({ data }) => {
+//   const parts = data.date.match(/(\d+)/g);
+//   const dipDate = new Date(Date.UTC(parts[2], parts[1] - 1, parts[0]));
 
-  let scrapeData = pastScrapeData.find(
-    ({ procedureId }) => procedureId === data.id
-  );
-  if (!scrapeData) {
-    scrapeData = { updatedAt: 0 };
-  }
-  const scrapeDate = new Date(scrapeData.updatedAt);
+//   let scrapeData = pastScrapeData.find(
+//     ({ procedureId }) => procedureId === data.id
+//   );
+//   if (!scrapeData) {
+//     scrapeData = { updatedAt: 0 };
+//   }
+//   const scrapeDate = new Date(scrapeData.updatedAt);
 
-  const timeSpanDib = new Date() - dipDate;
-  const timeSpanScrape = new Date() - scrapeDate;
+//   const timeSpanDib = new Date() - dipDate;
+//   const timeSpanScrape = new Date() - scrapeDate;
 
-  const oneDay = 1000 * 60 * 60 * 24;
-  const oneWeek = oneDay * 7;
-  const oneMonth = oneDay * 31;
-  const oneYear = oneDay * 365;
+//   const oneDay = 1000 * 60 * 60 * 24;
+//   const oneWeek = oneDay * 7;
+//   const oneMonth = oneDay * 31;
+//   const oneYear = oneDay * 365;
 
-  // TIME SCRAPE
-  if (
-    // always scrape when dib_date is after last scrape_date
-    dipDate > scrapeDate ||
-    // always scrape last 3 Months
-    timeSpanDib < 3 * oneMonth ||
-    // always scrape when last scrape_date is one month old
-    timeSpanScrape > oneMonth ||
-    // always scrape when last scrape_date is one day old and dib is up to 1 year old
-    (timeSpanScrape > oneDay && timeSpanDib < oneYear) ||
-    // always scrape when last scrape_date is one week old and dib is up to 4 year old
-    (timeSpanScrape > oneWeek && timeSpanDib < 4 * oneYear)
-  ) {
-    return true;
-  }
+//   // TIME SCRAPE
+//   if (
+//     // always scrape when dib_date is after last scrape_date
+//     dipDate > scrapeDate ||
+//     // always scrape last 3 Months
+//     timeSpanDib < 3 * oneMonth ||
+//     // always scrape when last scrape_date is one month old
+//     timeSpanScrape > oneMonth ||
+//     // always scrape when last scrape_date is one day old and dib is up to 1 year old
+//     (timeSpanScrape > oneDay && timeSpanDib < oneYear) ||
+//     // always scrape when last scrape_date is one week old and dib is up to 4 year old
+//     (timeSpanScrape > oneWeek && timeSpanDib < 4 * oneYear)
+//   ) {
+//     return true;
+//   }
 
-  // STATUS SCRAPE -> Whitelist
-  if (
-    procedureStatusWhitelist.find(white => white === scrapeData.currentStatus)
-  ) {
-    return true;
-  }
+//   // STATUS SCRAPE -> Whitelist
+//   if (
+//     procedureStatusWhitelist.find(white => white === scrapeData.currentStatus)
+//   ) {
+//     return true;
+//   }
 
-  return false;
-};
+//   return false;
+// };
 
 let linksSum = 0;
 let startDate;
@@ -313,7 +313,7 @@ const cronTask = async () => {
           .post(`${CONSTANTS.DEMOCRACY_SERVER_WEBHOOK_URL}`, {
             data: groups
           })
-          .then(async response => {
+          .then(async () => {
             await CronJobModel.update(
               {
                 name: "import-procedures"
