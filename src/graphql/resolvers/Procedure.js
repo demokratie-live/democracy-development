08a21862a317b1efa687267e34cac5cd932750d5
Manuel Ruck <github@manuelruck.de>
2018-08-29 15:08:59 +0200

    fix saving & update procedure

--- src/graphql/resolvers/Procedure.js
+++ src/graphql/resolvers/Procedure.js
@@ -145,13 +145,12 @@ export default {
       );
 
       axios
-        .post(`${CONSTANTS.DEMOCRACY_SERVER_WEBHOOK_URL}`, {
-          data: [
-            {
-              period: procedure.period,
-              types: [{ type: procedure.type, changedIds: [procedure.procedureId] }],
-            },
-          ],
+        .post(`${CONSTANTS.DEMOCRACY_SERVER_WEBHOOK_URL}Procedures`, {
+          data: {
+            procedureIds: [procedure.procedureId],
+            name: 'ChangeVoteData',
+          },
+
           timeout: 1000 * 60 * 5,
         })
         .then(async response => {
