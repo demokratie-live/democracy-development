def3a91133ac928e4bfbe90fe8d7c8d4c8b1f878
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2018-08-30 15:37:13 +0200

    missing changes gitbranch.sh

--- .travis/gitbranch.sh
+++ .travis/gitbranch.sh
@@ -3,8 +3,33 @@
 # Usage description
 usage() { echo "Usage: $0 [-d <string>] [-b <string>]" 1>&2; exit 1; }
 
-- cd client
-    - COMMIT=$(git rev-parse HEAD)
-    - BRANCHES=$(git branch -r --contains ${COMMIT})
-    - ORIGINMASTER='origin/master'
-    - if `echo ${BRANCHES} | grep "${ORIGINMASTER}" 1>/dev/null 2>&1`; then exit 0; else exit 1; fi
+# Parse Options
+while getopts ":d:b:" o; do
+    case "${o}" in
+        d)
+            directory=${OPTARG}
+            ;;
+        b)
+            branch=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${directory}" ] || [ -z "${branch}" ]; then
+    usage
+fi
+
+cd $directory
+
+COMMIT=$(git rev-parse HEAD)
+BRANCHES=$(git branch -r --contains ${COMMIT})
+
+if `echo ${BRANCHES} | grep "${branch}" 1>/dev/null 2>&1`; then
+  exit 0;
+else
+  exit 1;
+fi
