85498a57a5094c8537694f0931410789dabdaf66
Manuel Ruck <github@manuelruck.de>
2018-10-11 17:38:32 +0200

    don’t show „Zurückgezogen“ in admin area

--- src/graphql/resolvers/Procedure.js
+++ src/graphql/resolvers/Procedure.js
@@ -89,6 +89,7 @@ export default {
               currentStatus: { $in: PROCEDURE_STATES.COMPLETED },
             },
           ],
+          currentStatus: { $ne: 'Zurückgezogen' },
         };
         return ProcedureModel.find({ ...match })
           .sort({ createdAt: 1 })
