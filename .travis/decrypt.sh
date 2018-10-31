c875e03cf986aba397861c0f32cb3f3b0b07c80e
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2018-10-31 15:53:13 +0100

    corrected names of repos & services in docker, travis & decrpt env files

--- .travis/decrypt.sh
+++ .travis/decrypt.sh
@@ -8,9 +8,9 @@ openssl aes-256-cbc -k "$SECRET" -in ./.travis/ssh_key.enc -out ./.travis/ssh_ke
 openssl aes-256-cbc -k "$SECRET" -in ./.travis/ssh_key.pub.enc -out ./.travis/ssh_key.pub -d
 
 # Server .env
-openssl aes-256-cbc -k "$SECRET" -in ./server/.env.enc -out ./server/.env -d
+openssl aes-256-cbc -k "$SECRET" -in ./democracy-app.de-api/.env.enc -out ./democracy-app.de-api/.env -d
 # Server-Admin .env
-openssl aes-256-cbc -k "$SECRET" -in ./server-admin/.env.enc -out ./server-admin/.env -d
+openssl aes-256-cbc -k "$SECRET" -in ./democracy-app.de-admin/.env.enc -out ./democracy-app.de-admin/.env -d
 
 # Bundestag.io .env
 openssl aes-256-cbc -k "$SECRET" -in ./bundestag.io/.env.enc -out ./bundestag.io/.env -d
