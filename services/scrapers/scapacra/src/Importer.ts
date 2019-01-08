4fdace44bf9934eddc5c5cfdf07b858d38b993e4
Jannik Rebmann <jannik.rebmann@posteo.de>
2019-01-08 19:04:01 +0100

    Added IDataPackage (Data + Metadata)

--- services/scrapers/scapacra/src/Importer.ts
+++ services/scrapers/scapacra/src/Importer.ts
@@ -19,7 +19,7 @@ namespace Documents_Importer {
     /**
      * The browser navigates through a document structure and retrieves the desired fragments for the parser.
      */
-    export interface IBrowser<T extends DataType> extends IterableIterator<Promise<T>> {
+    export interface IBrowser<T extends DataType> extends IterableIterator<Promise<IDataPackage<T>>> {
     }
 
     /**
@@ -29,7 +29,7 @@ namespace Documents_Importer {
         /**
          * Extracts the data as JSON from a given data format.
          */
-        parse(content: T): Promise<JSON[]>;
+        parse(data: IDataPackage<T>): Promise<IDataPackage<JSON>[]>;
     }
 
     /**
@@ -48,10 +48,60 @@ namespace Documents_Importer {
     }
 
     /**
+     * Definement of global metadata information.
+     * Can always be extended with other information.
+     */
+    export interface IMetadata {
+        /**
+         * Provides a short description.
+         */
+        name?: string;
+        
+        /**
+         * Provides information about the source of the data.  
+         */
+        url?: string;
+
+        /**
+         * Provides a description about the data.
+         */
+        description?: string;
+
+        /**
+         * Provides tags optimizing the data searches.
+         */
+        tags?: string[];
+    }
+
+    /**
+     * A data bundle decripes all forms of information it's their metadata.
+     * <T>: Datatype of the wrapped data. 
+     */
+    export interface IDataPackage<T> {
+        /**
+         * See interface IMetadata.
+         */
+        metadata: IMetadata;
+
+        /**
+         * Raw data.
+         */
+        data: T;
+    }    
+
+    /**
      * A scraper executes multiple parser processes defined by scraper configurations and return their collected results over a central callback.
      */
     export class Scraper {
-        public static async scrape<T extends DataType>(configs: IScraperConfiguration<T>[], callback: (jsons: any[]) => void) {
+
+        /**
+         * The scraper shedules the execution of the browsers with their coresponding parsers.
+         * It collects the browsed and parsed data and returns it by a simple callback function.  
+         * 
+         * @param configs   Bundle of configurations with information about the IBrowser and IParser combination to be executed.  
+         * @param callback  Callback lambda for retrieving the scraper results.
+         */
+        public static async scrape<T extends DataType>(configs: IScraperConfiguration<T>[], callback: (data: IDataPackage<any>[]) => void) {
             for (const config of configs) {
                 let browser = config.getBrowser();
                 let parser = config.getParser();
