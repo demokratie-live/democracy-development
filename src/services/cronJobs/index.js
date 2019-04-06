4f28694896477bc297136f28eaf4392d73ac5beb
Manuel Ruck <github@manuelruck.de>
2019-04-06 11:18:22 +0200

    run bundestag.io crons only one time a day in the night

--- src/services/cronJobs/index.js
+++ src/services/cronJobs/index.js
@@ -1,17 +1,75 @@
 import { CronJob } from 'cron';
 
+import CONFIG from '../../config';
+
 import importProcedures from './../../importer/importProcedures';
 import importAgenda from './../../importer/importAgenda';
 import importNamedPolls from './../../importer/importNamedPolls';
 import importNamedPollDeputies from './../../importer/importNamedPollDeputies';
 import importDeputyProfiles from './../../importer/importDeputyProfiles';
 
-const cronJobs = () => [
-  new CronJob('15 * * * *', importProcedures, null, true, 'Europe/Berlin', null, true),
-  new CronJob('*/15 * * * *', importAgenda, null, true, 'Europe/Berlin', null, true),
-  new CronJob('0 * * * *', importNamedPolls, null, true, 'Europe/Berlin', null, true),
-  new CronJob('15 * * * *', importNamedPollDeputies, null, true, 'Europe/Berlin', null, true),
-  new CronJob('30 * * * *', importDeputyProfiles, null, true, 'Europe/Berlin', null, true),
-];
+const jobs = [];
+if (CONFIG.CRON_PROCEDURES)
+  jobs.push(
+    new CronJob(
+      CONFIG.CRON_PROCEDURES,
+      importProcedures,
+      null,
+      true,
+      'Europe/Berlin',
+      null,
+      CONFIG.CRON_START_ON_INIT,
+    ),
+  );
+if (CONFIG.CRON_AGENDA)
+  jobs.push(
+    new CronJob(
+      CONFIG.CRON_AGENDA,
+      importAgenda,
+      null,
+      true,
+      'Europe/Berlin',
+      null,
+      CONFIG.CRON_START_ON_INIT,
+    ),
+  );
+if (CONFIG.CRON_NAMED_POLLS)
+  jobs.push(
+    new CronJob(
+      CONFIG.CRON_NAMED_POLLS,
+      importNamedPolls,
+      null,
+      true,
+      'Europe/Berlin',
+      null,
+      CONFIG.CRON_START_ON_INIT,
+    ),
+  );
+if (CONFIG.CRON_NAMED_POLLS_DEPUTIES)
+  jobs.push(
+    new CronJob(
+      CONFIG.CRON_NAMED_POLLS_DEPUTIES,
+      importNamedPollDeputies,
+      null,
+      true,
+      'Europe/Berlin',
+      null,
+      CONFIG.CRON_START_ON_INIT,
+    ),
+  );
+if (CONFIG.CRON_DEPUTY_PROFILES)
+  jobs.push(
+    new CronJob(
+      CONFIG.CRON_DEPUTY_PROFILES,
+      importDeputyProfiles,
+      null,
+      true,
+      'Europe/Berlin',
+      null,
+      CONFIG.CRON_START_ON_INIT,
+    ),
+  );
+
+const cronJobs = () => jobs;
 
 module.exports = cronJobs;
