288d1e1ea2820b8e86626af71010d418495f7470
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2019-10-14 15:46:49 +0200

    prune docker images after start of new containers

--- restart-production.sh
+++ restart-production.sh
@@ -2,3 +2,4 @@
 echo "### Start docker Process ###"
 echo "# docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build $@"
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build $@
+docker system prune --all --force
