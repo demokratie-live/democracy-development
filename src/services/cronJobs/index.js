14c2b2c0b05fe6d158fb03d26dcf4562470d0876
ManAnRuck <github@manuelruck.de>
2020-07-02 11:51:23 +0200

    decouple import-named-polls
    Signed-off-by: ManAnRuck <github@manuelruck.de>

--- src/services/cronJobs/index.js
+++ src/services/cronJobs/index.js
@@ -3,15 +3,9 @@ import { CronJob } from 'cron';
 import { resetCronRunningState } from '@democracy-deutschland/bundestagio-common';
 import CONFIG from '../../config';
 
-import importNamedPolls, {
-  CRON_NAME as CRON_NAME_NAMED_POLLS,
-} from '../../importer/importNamedPolls';
 import importNamedPollDeputies, {
   CRON_NAME as CRON_NAME_NAMED_POLLS_DEPUTIES,
 } from '../../importer/importNamedPollDeputies';
-import importDeputyProfiles, {
-  CRON_NAME as CRON_NAME_DEPUTY_PROFILES,
-} from '../../importer/importDeputyProfiles';
 
 // global variable to store cronjobs
 const jobs = [];
@@ -29,13 +23,6 @@ const cronJobs = async () => {
   // Server freshly started -> Reset all cron states
   // This assumes that only one instance is running on the same database
   await resetCronRunningState();
-  // NamedPolls
-  registerCronJob(
-    CRON_NAME_NAMED_POLLS,
-    CONFIG.CRON_NAMED_POLLS,
-    importNamedPolls,
-    CONFIG.CRON_START_ON_INIT,
-  );
   // NamedPollsDeputies
   registerCronJob(
     CRON_NAME_NAMED_POLLS_DEPUTIES,
