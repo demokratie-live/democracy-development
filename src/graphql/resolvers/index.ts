b7e93d96e9e7e27b88287c9c3d590288a30b2143
ManAnRuck <github@manuelruck.de>
2020-07-17 16:23:05 +0200

    fix resolvers lint issue
    Signed-off-by: ManAnRuck <github@manuelruck.de>

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,11 +1,8 @@
-import { mergeResolvers } from 'merge-graphql-schemas';
 import ConferenceWeekDetail from './ConferenceWeekDetail';
 import Date from './Date';
 import Deputy from './Deputy';
 import NamedPoll from './NamedPoll';
 import Procedure from './Procedure';
+import _ from 'lodash';
 
-// const resolversArray = fileLoader(path.join(__dirname, './'), { extensions: ['.js'] });
-const resolversArray = [ConferenceWeekDetail, Date, Deputy, NamedPoll, Procedure];
-
-export default mergeResolvers(resolversArray);
+export default _.merge({}, ConferenceWeekDetail, Date, Deputy, NamedPoll, Procedure);
