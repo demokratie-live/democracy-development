89f169424c08828d3abe60a108a963aa503d3f2a
ManAnRuck <github@manuelruck.de>
2021-05-23 00:22:22 +0200

    fix deprecated warnings & improve logs
    Signed-off-by: ManAnRuck <github@manuelruck.de>

---
+++ services/cron-jobs/queue-pushs-vote-top-100/src/config.ts
@@ -0,0 +1,1 @@
+export const DEV_MODE = process.env.DEV_MODE === "true";
--- services/cron-jobs/queue-pushs-vote-top-100/src/mongoose.ts
+++ services/cron-jobs/queue-pushs-vote-top-100/src/mongoose.ts
@@ -1,6 +1,6 @@
 import { mongoose } from "@democracy-deutschland/democracy-common";
 export default () =>
-  new Promise<void>(async (resolve, reject) => {
+  new Promise<void>(async (resolve) => {
     mongoose.set("useFindAndModify", false);
     // Mongo Debug
     mongoose.set("debug", false);
