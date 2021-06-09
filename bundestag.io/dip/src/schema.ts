cd8e94b05ea45c718682982717a09014eb3ee239
Robert Schäfer <git@roschaefer.de>
2021-06-10 01:25:31 +0200

    basic DIP schema

---
+++ bundestag.io/dip/src/schema.ts
@@ -0,0 +1,18 @@
+import { gql } from 'apollo-server'
+export default gql`
+type Query {
+  procedures(offset: Int, limit: Int): [Procedure]
+}
+scalar Date
+
+type Procedure {
+  id: ID!
+  titel: String!
+  beratungsstand: String
+  vorgangstyp: String
+  typ: String
+  wahlperiode: Int
+  initiative: [String]
+  datum: Date
+}
+`
