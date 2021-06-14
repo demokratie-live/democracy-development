b2aedbaf075be1a058a19936b0628dd7c9139759
Robert Schäfer <git@roschaefer.de>
2021-06-14 13:49:53 +0200

    refactor: DRY config.ts
    as suggested by @mananruck
    
    Co-authored-by: ManAnRuck <m.ruck@democracy-deutschland.de>

--- services/cron-jobs/crawler/src/config.ts
+++ services/cron-jobs/crawler/src/config.ts
@@ -1,2 +1,4 @@
-export const { MONGO_DB_URL } = process.env
-export const { DIP_GRAPHQL_ENDPOINT= 'http://localhost:3101'} = process.env
+export const { 
+    MONGO_DB_URL, 
+    DIP_GRAPHQL_ENDPOINT = "http://localhost:3101" 
+} = process.env;
