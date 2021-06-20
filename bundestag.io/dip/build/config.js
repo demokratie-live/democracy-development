775e83e4b66ac4ac641d6520982eefd25c6f4bbe
Robert Schäfer <git@roschaefer.de>
2021-06-20 19:42:48 +0200

    refactor: remove .gitignored folders

--- bundestag.io/dip/build/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var _a, _b, _c;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PORT = exports.DIP_API_ENDPOINT = exports.DIP_API_KEY = void 0;
-_a = process.env, _b = _a.DIP_API_KEY, exports.DIP_API_KEY = _b === void 0 ? '' : _b, _c = _a.DIP_API_ENDPOINT, exports.DIP_API_ENDPOINT = _c === void 0 ? 'https://search.dip.bundestag.de' : _c;
-exports.PORT = process.env.PORT ? parseInt(process.env.PORT) : 3101;
