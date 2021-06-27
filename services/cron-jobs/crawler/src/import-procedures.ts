518c01eb2d561aeda62c0ffb160b8ea5b8ce8f4e
ManAnRuck <github@manuelruck.de>
2021-06-27 17:39:27 +0200

    Throw error instead of exit(1)
    to see final error in production without enableing debug mode (maybe until we have an stable logging system)
    
    Signed-off-by: ManAnRuck <github@manuelruck.de>

--- services/cron-jobs/crawler/src/import-procedures.ts
+++ services/cron-jobs/crawler/src/import-procedures.ts
@@ -11,56 +11,56 @@ log.log = console.log.bind(console);
 const { DIP_GRAPHQL_ENDPOINT, IMPORT_PROCEDURES_START_CURSOR, IMPORT_PROCEDURES_CHUNK_SIZE, IMPORT_PROCEDURES_CHUNK_ROUNDS, IMPORT_PROCEDURES_FILTER_BEFORE, IMPORT_PROCEDURES_FILTER_AFTER } = config;
 
 const procedureQuery = gql`
-query($cursor: String, $offset: Int, $limit: Int, $filter: ProcedureFilter) {
+  query ($cursor: String, $offset: Int, $limit: Int, $filter: ProcedureFilter) {
   procedures(cursor: $cursor, offset: $offset, limit: $limit, filter: $filter) {
-    edges {
-      node {
-        abstract
-        procedureId
-        currentStatus
-        type
-        period
-        title
-        date
-        subjectGroups
-        tags
-        history {
-          assignment
-          initiator
-          findSpot
-          findSpotUrl
-          voteDate: date
-          decision {
-            page
-            tenor
-            document
-            comment
+      edges {
+        node {
+          abstract
+          procedureId
+          currentStatus
+          type
+          period
+          title
+          date
+          subjectGroups
+          tags
+          history {
+            assignment
+            initiator
+            findSpot
+            findSpotUrl
+            voteDate: date
+            decision {
+              page
+              tenor
+              document
+              comment
+              type
+              foundation
+              majority
+            }
+          }
+          importantDocuments {
+            editor
+            number
             type
-            foundation
-            majority
+            url
+          }
+          plenums {
+            editor
+            number
+            link
+            pages
           }
         }
-        importantDocuments {
-          editor
-          number
-          type
-          url
-        }
-        plenums {
-          editor
-          number
-          link
-          pages
-        }
+      }
+      totalCount
+      pageInfo {
+        hasNextPage
+        endCursor
       }
     }
-    totalCount
-    pageInfo {
-      hasNextPage
-      endCursor
-    }
   }
-}
 `
 
 export default async function importProcedures() {
@@ -78,7 +78,7 @@ export default async function importProcedures() {
     log(`Round ${round} - Cursor ${variables.cursor}`)
     const { procedures: { edges, pageInfo: { endCursor, hasNextPage } } } = await request(DIP_GRAPHQL_ENDPOINT, procedureQuery, variables );
     await ProcedureModel.bulkWrite(
-      edges.map((edge: { node: {  procedureId: string } }) => ({
+      edges.map((edge: { node: { procedureId: string } }) => ({
         updateOne: {
           filter: { procedureId: edge.node.procedureId },
           update: edge.node,
@@ -97,9 +97,9 @@ export default async function importProcedures() {
     await mongoConnect();
     await importProcedures()
     process.exit(0);
-  } catch(err) {
+  } catch (err) {
     error(err)
-    process.exit(1)
+    throw err;
   } finally {
     mongoDisconnect()
   }
