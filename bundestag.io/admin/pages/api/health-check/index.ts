d42e35d03fc91ee6f00f56400c6a010d33360c96
ManAnRuck <github@manuelruck.de>
2021-07-10 18:32:26 +0200

    add health-check
    Signed-off-by: ManAnRuck <github@manuelruck.de>

---
+++ bundestag.io/admin/pages/api/health-check/index.ts
@@ -0,0 +1,7 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).send("ok");
+};
+
+export default handler;
