41d28c9e133e245ec3e0db0ac0492a1604889e95
ManAnRuck <github@manuelruck.de>
2021-07-10 17:42:42 +0200

    fix graphql proxy
    Signed-off-by: ManAnRuck <github@manuelruck.de>

--- bundestag.io/admin/next.config.babel.ts
+++ bundestag.io/admin/next.config.babel.ts
@@ -2,17 +2,6 @@ import type { NextConfig } from "next/dist/next-server/server/config";
 
 const nextConfig: Partial<NextConfig> = {
   reactStrictMode: true,
-
-  //   poweredByHeader: false,
-
-  async rewrites() {
-    return [
-      {
-        source: "/graphql",
-        destination: process.env.BUNDESTAGIO_SERVER_URL,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
