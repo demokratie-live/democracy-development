70b1a58ee123cb4a069a58f1e6d75c62fc95c6d9
Robert Schäfer <git@roschaefer.de>
2021-07-11 15:07:48 +0200

    feat(crawler): setup eslint+prettier

--- services/cron-jobs/crawler/src/config.ts
+++ services/cron-jobs/crawler/src/config.ts
@@ -1,21 +1,19 @@
-let {
-  MONGO_DB_URL = "mongodb://localhost:27017/bundestagio",
-  DIP_GRAPHQL_ENDPOINT = "http://localhost:3101",
-  IMPORT_PROCEDURES_START_CURSOR = "*",
-  IMPORT_PROCEDURES_CHUNK_SIZE = 100,
-  IMPORT_PROCEDURES_CHUNK_ROUNDS = 5,
+const {
+  MONGO_DB_URL = 'mongodb://localhost:27017/bundestagio',
+  DIP_GRAPHQL_ENDPOINT = 'http://localhost:3101',
+  IMPORT_PROCEDURES_START_CURSOR = '*',
   IMPORT_PROCEDURES_FILTER_BEFORE = new Date().toISOString().slice(0, 10),
-  IMPORT_PROCEDURES_FILTER_AFTER = new Date(
-    Number(new Date()) - 1000 * 60 * 60 * 24 * 7 * 4
-  )
+  IMPORT_PROCEDURES_FILTER_AFTER = new Date(Number(new Date()) - 1000 * 60 * 60 * 24 * 7 * 4)
     .toISOString()
     .slice(0, 10),
 } = process.env;
+
+let { IMPORT_PROCEDURES_CHUNK_SIZE = 100, IMPORT_PROCEDURES_CHUNK_ROUNDS = 5 } = process.env;
+
 IMPORT_PROCEDURES_CHUNK_SIZE = Number(IMPORT_PROCEDURES_CHUNK_SIZE);
 IMPORT_PROCEDURES_CHUNK_ROUNDS = Number(IMPORT_PROCEDURES_CHUNK_ROUNDS);
-const IMPORT_PROCEDURES_FILTER_TYPES = process.env
-  .IMPORT_PROCEDURES_FILTER_TYPES
-  ? process.env.IMPORT_PROCEDURES_FILTER_TYPES.split(",")
+const IMPORT_PROCEDURES_FILTER_TYPES = process.env.IMPORT_PROCEDURES_FILTER_TYPES
+  ? process.env.IMPORT_PROCEDURES_FILTER_TYPES.split(',')
   : undefined;
 
 export default {
