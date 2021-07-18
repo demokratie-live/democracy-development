1e563d8945bc45a89d11ddf1086b41a33857f664
Robert Schäfer <git@roschaefer.de>
2021-07-18 21:46:37 +0200

    lint: add eslint-plugin-jest
    This package really helps to find e.g. `it.omit` tests that disable
    other test cases.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
-  "extends": ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  "extends": ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended", "plugin:jest/recommended"],
   "parser": "@typescript-eslint/parser",
-  "plugins": ["@typescript-eslint"],
+  "plugins": ["@typescript-eslint", "jest"],
   "rules": {
     "@typescript-eslint/no-unused-vars": "error",
-    "@typescript-eslint/no-explicit-any": "error"
+    "@typescript-eslint/no-explicit-any": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/prefer-to-contain": "warn"
   }
 };
