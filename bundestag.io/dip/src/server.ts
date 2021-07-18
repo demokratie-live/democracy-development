309a1adbd9be7e550df9a6f37fc012391a1e7f54
Robert Schäfer <git@roschaefer.de>
2021-07-18 20:44:47 +0200

    lint: fix lints in current workspaces

--- bundestag.io/dip/src/server.ts
+++ bundestag.io/dip/src/server.ts
@@ -1,17 +1,25 @@
-import express from 'express'
-import { ApolloServer } from 'apollo-server-express'
-import DipAPI from './DipAPI'
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import DipAPI from './DipAPI';
 import typeDefs from './schema';
-import resolvers from './resolvers'
+import resolvers from './resolvers';
 
-export default function createServer({ DIP_API_ENDPOINT, DIP_API_KEY, RATE_LIMIT }: { DIP_API_ENDPOINT: string, DIP_API_KEY: string, RATE_LIMIT: number}) {
+export default function createServer({
+  DIP_API_ENDPOINT,
+  DIP_API_KEY,
+  RATE_LIMIT,
+}: {
+  DIP_API_ENDPOINT: string;
+  DIP_API_KEY: string;
+  RATE_LIMIT: number;
+}): { app: express.Express; server: ApolloServer } {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     dataSources: () => ({ dipAPI: new DipAPI({ baseURL: DIP_API_ENDPOINT, limit: RATE_LIMIT }) }),
-    context: () => ({ DIP_API_KEY,  }),
+    context: () => ({ DIP_API_KEY }),
   });
   const app = express();
-  server.applyMiddleware({ app, path: '/' })
-  return { app, server }
+  server.applyMiddleware({ app, path: '/' });
+  return { app, server };
 }
