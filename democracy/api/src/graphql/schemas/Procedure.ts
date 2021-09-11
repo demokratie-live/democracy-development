776bd88a105f7f18b4a7a333463f767b50f90b91
ManAnRuck <github@manuelruck.de>
2021-09-11 15:31:14 +0200

    make period for procedures query dynamic
    Signed-off-by: ManAnRuck <github@manuelruck.de>

--- democracy/api/src/graphql/schemas/Procedure.ts
+++ democracy/api/src/graphql/schemas/Procedure.ts
@@ -94,7 +94,7 @@ type RecommendedProceduresResult {
 type Query {
   procedure(id: ID!): Procedure!
   ${/* DEPRECATED listType 2019-01-29 Renamed filed VOTING to PAST and IN_VOTE */ ''}
-  procedures(listTypes: [ListType!], type: ProcedureType, pageSize: Int, offset: Int, sort: String, filter: ProcedureFilter): [Procedure!]!
+  procedures(listTypes: [ListType!], type: ProcedureType, pageSize: Int, offset: Int, sort: String, filter: ProcedureFilter, period: Int): [Procedure!]!
   proceduresById(ids: [String!]!, pageSize: Int, offset: Int): [Procedure!]!
   proceduresByIdHavingVoteResults(procedureIds: [String!], timespan: VotedTimeSpan, pageSize: Int, offset: Int, filter: ProcedureWOMFilter): ProceduresHavingVoteResults!
   notifiedProcedures: [Procedure!]!
