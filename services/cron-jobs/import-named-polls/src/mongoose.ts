c8dbfba2d61df75e3ca99e9f67c8df7e71fc2d2c
ManAnRuck <github@manuelruck.de>
2021-11-21 17:18:59 +0100

    improve named poll scraper
    Signed-off-by: ManAnRuck <github@manuelruck.de>

--- services/cron-jobs/import-named-polls/src/mongoose.ts
+++ services/cron-jobs/import-named-polls/src/mongoose.ts
@@ -1,21 +1,17 @@
-import { mongoose } from "@democracy-deutschland/bundestagio-common";
+import { mongoose } from '@democracy-deutschland/bundestagio-common';
 
 export default () =>
   new Promise(async (resolve, reject) => {
-    mongoose.set("useFindAndModify", false);
     // Mongo Debug
-    mongoose.set("debug", false);
+    mongoose.set('debug', false);
 
-    mongoose.connect(process.env.DB_URL!, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    mongoose.connect(process.env.DB_URL!);
 
-    mongoose.connection.once("connected", () => {
-      console.info("MongoDB is running");
-      resolve();
+    mongoose.connection.once('connected', () => {
+      console.info('MongoDB is running');
+      resolve(true);
     });
-    mongoose.connection.on("error", (e: Error) => {
+    mongoose.connection.on('error', (e: Error) => {
       // Unknown if this ends up in main - therefore we log here
       console.error(e.stack);
       throw e;
