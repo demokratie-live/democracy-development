dd9539d96bfdaa38817d59751c802f30ca6aa526
ManAnRuck <github@manuelruck.de>
2021-12-12 14:03:10 +0100

    queue-pushs-conference-week memory fix
    run get devices in loop
    
    Signed-off-by: ManAnRuck <github@manuelruck.de>

--- services/cron-jobs/queue-pushs-conference-week/src/index.ts
+++ services/cron-jobs/queue-pushs-conference-week/src/index.ts
@@ -1,5 +1,5 @@
-import mongoConnect from "./mongoose";
-import moment from "moment";
+import mongoConnect from './mongoose';
+import moment from 'moment';
 
 import {
   ProcedureModel,
@@ -10,7 +10,7 @@ import {
   setCronStart,
   setCronSuccess,
   queuePushs,
-} from "@democracy-deutschland/democracy-common";
+} from '@democracy-deutschland/democracy-common';
 
 const start = async () => {
   /*
@@ -19,51 +19,69 @@ const start = async () => {
   (Sonntag vor Sitzungswoche, alle)
   */
 
-  const CRON_NAME = "queuePushsConferenceWeek";
+  const CRON_NAME = 'queuePushsConferenceWeek';
   const startDate = new Date();
   let counter = 0;
   await setCronStart({ name: CRON_NAME, startDate });
   // Find coresponding Procedures
-  const startOfWeek = moment().startOf("week").toDate(); // Should be So
-  const endOfWeek = moment().endOf("week").toDate(); // Should be Sa
+  const startOfWeek = moment().startOf('week').toDate(); // Should be So
+  const endOfWeek = moment().endOf('week').toDate(); // Should be Sa
+
+  console.log('Find Procedures between', startOfWeek, 'and', endOfWeek);
   const procedures = await ProcedureModel.find(
     {
-      $and: [
-        { voteDate: { $gte: startOfWeek } },
-        { voteDate: { $lte: endOfWeek } },
-      ],
+      $and: [{ voteDate: { $gte: startOfWeek } }, { voteDate: { $lte: endOfWeek } }],
     },
-    { procedureId: 1 }
+    { procedureId: 1 },
   );
   const procedureIds = procedures.map((p) => p.procedureId);
+  console.log('Found', procedureIds.length, 'Procedures');
 
   // Find Devices & Tokens
-  const devices = await DeviceModel.find({
-    "notificationSettings.enabled": true,
-    "notificationSettings.conferenceWeekPushs": true,
-  });
 
-  const tokens = devices.reduce<Device["pushTokens"]>(
-    (array, { pushTokens }) => [...array, ...pushTokens],
-    []
-  );
+  let hasMore = true;
+  let skip = 0;
+  let limit = 1000;
+  while (hasMore) {
+    console.log('Find Devices', skip);
+    const devices = await DeviceModel.find(
+      {
+        'notificationSettings.enabled': true,
+        'notificationSettings.conferenceWeekPushs': true,
+      },
+      { pushTokens: 1 },
+      { skip, limit },
+    );
 
-  // Only send Message if at least one vote & one token is found
-  if (tokens.length > 0 && procedureIds.length > 0) {
-    counter += tokens.length;
-    const title = "Kommende Woche ist Sitzungswoche!";
-    const message =
-      procedureIds.length === 1
-        ? `Es wartet 1 spannendes Thema auf Dich. Viel Spaß beim Abstimmen.`
-        : `Es warten ${procedureIds.length} spannende Themen auf Dich. Viel Spaß beim Abstimmen.`;
-    await queuePushs({
-      type: PUSH_TYPE.PROCEDURE_BULK,
-      category: PUSH_CATEGORY.CONFERENCE_WEEK,
-      title,
-      message,
-      procedureIds,
-      tokens,
-    });
+    if (devices.length === 0) {
+      console.log('No Devices found');
+      hasMore = false;
+    } else {
+      skip += limit;
+      const tokens = devices.reduce<Device['pushTokens']>((array, { pushTokens }) => [...array, ...pushTokens], []);
+
+      // Only send Message if at least one vote & one token is found
+      const queuePromises = [];
+      if (tokens.length > 0 && procedureIds.length > 0) {
+        counter += tokens.length;
+        const title = 'Kommende Woche ist Sitzungswoche!';
+        const message =
+          procedureIds.length === 1
+            ? `Es wartet 1 spannendes Thema auf Dich. Viel Spaß beim Abstimmen.`
+            : `Es warten ${procedureIds.length} spannende Themen auf Dich. Viel Spaß beim Abstimmen.`;
+        queuePromises.push(
+          queuePushs({
+            type: PUSH_TYPE.PROCEDURE_BULK,
+            category: PUSH_CATEGORY.CONFERENCE_WEEK,
+            title,
+            message,
+            procedureIds,
+            tokens,
+          }),
+        );
+        await Promise.all(queuePromises);
+      }
+    }
   }
 
   console.log(`queued push's: ${counter}`);
@@ -72,19 +90,13 @@ const start = async () => {
 };
 
 (async () => {
-  console.info("START");
-  console.info(
-    "process.env",
-    process.env.BUNDESTAGIO_SERVER_URL,
-    process.env.DB_URL
-  );
+  console.info('START');
+  console.info('process.env', process.env.BUNDESTAGIO_SERVER_URL, process.env.DB_URL);
   if (!process.env.BUNDESTAGIO_SERVER_URL) {
-    throw new Error(
-      "you have to set environment variable: BUNDESTAGIO_SERVER_URL & DB_URL"
-    );
+    throw new Error('you have to set environment variable: BUNDESTAGIO_SERVER_URL & DB_URL');
   }
   await mongoConnect();
-  console.log("procedures", await ProcedureModel.countDocuments({}));
+  console.log('procedures', await ProcedureModel.countDocuments({}));
   await start().catch(() => process.exit(1));
   process.exit(0);
 })();
