96ba04487064c95949b32c3b8b1b8490cfd9f655
Manuel Ruck <git@manuelruck.de>
2022-03-15 20:42:18 +0100

    disable send sms

--- democracy/api/src/graphql/resolvers/Device.ts
+++ democracy/api/src/graphql/resolvers/Device.ts
@@ -4,26 +4,8 @@ import crypto from 'crypto';
 
 import CONFIG from '../../config';
 import { createTokens, headerToken } from '../../express/auth';
-import { sendSMS, statusSMS } from '../../services/sms';
 import { Resolvers, NotificationSettings } from '../../generated/graphql';
 import { logger } from '../../services/logger';
-
-const calculateResendTime = ({
-  latestCodeTime,
-  codesCount,
-  expires,
-}: {
-  latestCodeTime: number;
-  codesCount: number;
-  expires: number;
-}): Date =>
-  new Date(
-    Math.min(
-      expires,
-      latestCodeTime +
-        (ms(CONFIG.SMS_VERIFICATION_CODE_RESEND_BASETIME) / 1000) ** codesCount * 1000,
-    ),
-  );
 
 const DeviceApi: Resolvers = {
   Query: {
@@ -46,197 +28,14 @@ const DeviceApi: Resolvers = {
     // ************
     // REQUEST CODE
     // ************
-    requestCode: async (
-      parent,
-      { newPhone, oldPhoneHash },
-      { user, device, phone, PhoneModel, VerificationModel },
-    ) => {
+    requestCode: async (parent) => {
       logger.graphql('Device.mutation.requestCode');
-      // Check for SMS Verification
-      if (!CONFIG.SMS_VERIFICATION) {
-        return {
-          reason: 'SMS Verification is disabled!',
-          succeeded: false,
-        };
-      }
-
-      if (!oldPhoneHash && user.isVerified()) {
-        return {
-          reason: 'You are already verified!',
-          succeeded: false,
-        };
-      }
-
-      // check newPhone prefix & length, 3 prefix, min. length 10
-      if (newPhone.substr(0, 3) !== '+49' || newPhone.length < 12) {
-        return {
-          reason:
-            'newPhone is invalid - does not have the required length of min. 12 digits or does not start with countrycode +49',
-          succeeded: false,
-        };
-      }
-
-      // Check for invalid transfere
-      const newPhoneHash = crypto.createHash('sha256').update(newPhone).digest('hex');
-      const newPhoneDBHash = crypto.createHash('sha256').update(newPhoneHash).digest('hex');
-      const oldPhoneDBHash = oldPhoneHash
-        ? crypto.createHash('sha256').update(oldPhoneHash).digest('hex')
-        : null;
-      if (newPhoneHash === oldPhoneHash) {
-        return {
-          reason: 'newPhoneHash equals oldPhoneHash',
-          succeeded: false,
-        };
-      }
-
-      // Check for valid oldPhoneHash
-      if ((oldPhoneHash && !user.isVerified()) || (oldPhoneHash && !phone)) {
-        return {
-          reason: 'Provided oldPhoneHash is invalid',
-          succeeded: false,
-        };
-      }
-
-      let verification = await VerificationModel.findOne({
-        phoneHash: newPhoneDBHash,
-      });
-      if (!verification) {
-        verification = new VerificationModel({
-          phoneHash: newPhoneDBHash,
-        });
-      }
-
-      // Genrate Code
-      const minVal = 100000;
-      const maxVal = 999999;
-
-      let code: string = (Math.floor(Math.random() * (maxVal - minVal + 1)) + minVal).toString(); // eslint-disable-line
-      if (CONFIG.SMS_SIMULATE) {
-        code = '000000';
-      }
-
-      const now = new Date();
-      // Check if there is still a valid Code
-      const activeCode = verification.verifications?.find(({ expires }) => now < new Date(expires));
-      if (activeCode) {
-        // ***********
-        // Resend Code
-        // ***********
-        // Find Code Count & latest Code Time
-        const codesCount = activeCode.codes?.length || 0;
-        const latestCode = activeCode.codes?.reduce(
-          (max, p) => (p.time > max.time ? p : max),
-          activeCode.codes[0],
-        );
-
-        // Check code time
-        if (
-          latestCode &&
-          latestCode.time.getTime() +
-            ms(CONFIG.SMS_VERIFICATION_CODE_RESEND_BASETIME) ** codesCount >=
-            now.getTime()
-        ) {
-          return {
-            reason: 'You have to wait till you can request another Code',
-            resendTime: calculateResendTime({
-              latestCodeTime: latestCode.time.getTime(),
-              codesCount,
-              expires: new Date(activeCode.expires).getTime(),
-            }),
-            expireTime: activeCode.expires,
-            succeeded: false,
-          };
-        }
-
-        // Validate that the Number has recieved the Code
-        const smsstatus = await statusSMS(latestCode?.SMSID || '');
-        if (!smsstatus.succeeded && [102, 103].includes(smsstatus.code)) {
-          console.log('DEBUG latestCode', { latestCode, activeCode });
-          return {
-            reason: 'Your number seems incorrect, please correct it!',
-            succeeded: false,
-          };
-        } else if (!smsstatus.succeeded) {
-          // TODO better error handling
-          console.error('SMS ERROR', latestCode, smsstatus);
-        }
-
-        // Send SMS
-        const { status, SMSID } = await sendSMS(newPhone, code);
-
-        activeCode.codes?.push({
-          code,
-          time: now,
-          SMSID,
-        });
-        await verification.save();
-
-        // Check Status here to make sure the Verification request is saved
-        if (!status) {
-          return {
-            reason: 'Could not send SMS to given newPhone',
-            succeeded: false,
-          };
-        }
-
-        return {
-          succeeded: true,
-          resendTime: calculateResendTime({
-            latestCodeTime: now.getTime(),
-            codesCount: codesCount + 1,
-            expires: new Date(activeCode.expires).getTime(),
-          }),
-          expireTime: activeCode.expires,
-        };
-        // ***********
-        // Resend Code
-        // ********END
-      }
-
-      // Send SMS
-      const { status, SMSID } = await sendSMS(newPhone, code);
-
-      // Allow to create new user based on last usage
-      const verificationPhone = await PhoneModel.findOne({
-        phoneHash: newPhoneDBHash,
-      });
-      let allowNewUser = false; // Is only set if there was a user registered
-      if (
-        verificationPhone &&
-        verificationPhone.updatedAt <
-          new Date(now.getTime() - ms(CONFIG.SMS_VERIFICATION_NEW_USER_DELAY))
-      ) {
-        // Older then 6 Months
-        allowNewUser = true;
-      }
-
-      // Code expiretime
-      const expires = new Date(now.getTime() + ms(CONFIG.SMS_VERIFICATION_CODE_TTL));
-      verification.verifications?.push({
-        deviceHash: device.deviceHash,
-        oldPhoneHash: oldPhoneDBHash || '',
-        codes: [{ code, time: now, SMSID }],
-        expires,
-      });
-      await verification.save();
-
-      // Check Status here to make sure the Verification request is saved
-      if (!status) {
-        return {
-          reason: 'Could not send SMS to given newPhone',
-          succeeded: false,
-        };
-      }
 
       return {
-        allowNewUser,
-        resendTime: calculateResendTime({
-          latestCodeTime: now.getTime(),
-          codesCount: 1,
-          expires: expires.getTime(),
-        }),
-        expireTime: expires,
-        succeeded: true,
+        allowNewUser: false,
+        resendTime: 99,
+        expireTime: 99,
+        succeeded: false,
       };
     },
 
