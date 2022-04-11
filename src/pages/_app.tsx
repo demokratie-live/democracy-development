70f3f2dfc5e810edd44df9784365352286a13237
Grzegorz Leoniec <greg@app-interactive.de>
2022-04-11 20:15:40 +0200

    client side loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { AppProps } from 'next/app';
 
 import '../styles/global.css';
 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+
+  uri: 'https://api.democracy-app.de/',
+});
+
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <ApolloProvider client={client}>
+    <Component {...pageProps} />
+  </ApolloProvider>
 );
 
 export default MyApp;
