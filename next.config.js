751d05364f5753ec0004f5dd0a757f02380e7d8f
Manuel Ruck <git@manuelruck.de>
2022-05-19 18:16:27 +0200

    Dockerize production build
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 module.exports = withBundleAnalyzer({
+  experimental: {
+    outputStandalone: true,
+  },
   eslint: {
     dirs: ['.'],
   },
