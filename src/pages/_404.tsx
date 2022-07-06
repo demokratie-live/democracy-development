7fb3c7382404d6fba69e7618b09afb96046d7cc8
Grzegorz Leoniec <greg@app-interactive.de>
2022-07-06 19:56:52 +0200

    redirect to home page on error

--- src/pages/_404.tsx
+++ src/pages/_404.tsx
@@ -1,5 +1,15 @@
+import { useEffect } from 'react';
+
+import { useRouter } from 'next/router';
+
 import Empty from '@/components/molecules/Empty';
 
 export default function FourOhFour() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/');
+  });
+
   return <Empty />;
 }
