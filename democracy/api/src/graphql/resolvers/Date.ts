1bcc9a423605ce951dd4eb5dd5a3ecd34916e6db
Manuel Ruck <git@manuelruck.de>
2023-01-08 15:01:38 +0100

    Remove democracy/api from monorepo
    New repository is https://github.com/demokratie-live/democracy-api
    
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- democracy/api/src/graphql/resolvers/Date.ts
+++
@@ -1,8 +0,0 @@
-import GraphQLDate from 'graphql-date';
-import { Resolvers } from '../../generated/graphql';
-
-const DateApi: Resolvers = {
-  Date: GraphQLDate,
-};
-
-export default DateApi;
--- democracy/api/src/graphql/resolvers/index.ts
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-import { fileLoader, mergeResolvers } from 'merge-graphql-schemas';
-
-const resolversArray = fileLoader(path.join(__dirname, './'), { extensions: ['.js', '.ts'] });
-
-export default mergeResolvers(resolversArray);
