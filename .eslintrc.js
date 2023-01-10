03152e8cc475460bb17a532a39e618107dcb4a85
Manuel Ruck <git@manuelruck.de>
2023-01-10 18:59:06 +0100

    build: :rotating_light: fix project settings in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   parserOptions: {
-    project: `./democracy/api/tsconfig.json`,
+    project: `./tsconfig.json`,
   },
   extends: [
     'eslint:recommended',
