586fc60d1ad46f277a328f16f1348f3a6e3e20f5
Manuel Ruck <git@manuelruck.de>
2023-06-17 18:17:19 +0200

    add period to plenaryMinute
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- common/bundestagio/src/models/PlenaryMinute/schema.ts
+++ common/bundestagio/src/models/PlenaryMinute/schema.ts
@@ -1,22 +1,29 @@
-import { Schema, Document, SchemaTimestampsConfig } from "mongoose";
+import { Schema, Document, SchemaTimestampsConfig } from 'mongoose';
 
 export interface IPlenaryMinute {
   date: Date;
   meeting: number;
+  period: number;
   xml: string;
 }
 
-export type PlenaryMinuteDoc = IPlenaryMinute &
-  Document &
-  SchemaTimestampsConfig;
+export type PlenaryMinuteDoc = IPlenaryMinute & Document & SchemaTimestampsConfig;
 
 const PlenaryMinuteSchema = new Schema<PlenaryMinuteDoc>(
   {
     date: { type: Date, required: true, unique: true },
-    meeting: { type: Number, required: true, unique: true },
+    meeting: { type: Number, required: true, unique: false },
+    period: { type: Number, required: true, unique: false },
     xml: { type: String, required: true, unique: true },
   },
-  { timestamps: true }
+  { timestamps: true },
+);
+PlenaryMinuteSchema.index(
+  {
+    meeting: 1,
+    period: 1,
+  },
+  { unique: true },
 );
 
 export default PlenaryMinuteSchema;
