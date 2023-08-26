768ee19fb516df4a3a2e7130ef8e118dbec9088b
Manuel Ruck <git@manuelruck.de>
2023-08-26 16:20:25 +0200

    fix android assetlinks fingerprint
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- src/pages/api/assetlinks.ts
+++ src/pages/api/assetlinks.ts
@@ -27,7 +27,7 @@ const assetlinksStagingDevelopment = JSON.stringify([
       namespace: 'android_app',
       package_name: 'de.democracydeutschland.app.internal',
       sha256_cert_fingerprints: [
-        '56:C3:CC:61:4B:16:7C:61:D7:77:FB:12:E6:57:96:B4:04:F0:B4:03:4D:C1:0D:63:91:90:4B:67:E7:E7:5A:F5',
+        'FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C',
       ],
     },
   },
