d157922dcd7dcfd524a0ae4e17100cd679be3f06
Manuel Ruck <git@manuelruck.de>
2023-11-01 10:23:08 +0100

    fix: 🐛 add health endpoint
    for health checks
    
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- bundestag.io/api/src/index.ts
+++ bundestag.io/api/src/index.ts
@@ -1,5 +1,4 @@
-import { server } from './services/graphql';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { createServer } from './services/graphql';
 import {
   ProcedureModel,
   UserModel,
@@ -9,6 +8,11 @@ import {
   PlenaryMinuteModel,
   mongoConnect,
 } from '@democracy-deutschland/bundestagio-common';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { expressMiddleware } from '@apollo/server/express4';
+import bodyParser from 'body-parser';
 
 // *****************************************************************
 // IMPORTANT - you cannot include any models before migrating the DB
@@ -19,29 +23,65 @@ import CONFIG from './config';
 // Allow global Log
 import './services/logger';
 
+const app = express();
+const httpServer = http.createServer(app);
+const server = createServer({ httpServer });
+
 const main = async () => {
   // Connect to DB - this keeps the process running
   // IMPORTANT - This is done before any Model is registered
   await mongoConnect();
 
-  startStandaloneServer(server, {
-    listen: {
-      port: CONFIG.PORT,
-      path: CONFIG.GRAPHQL_PATH,
-    },
-    context: async ({ req }) => {
-      return {
-        req,
-        // Models
-        ProcedureModel,
-        UserModel,
-        DeputyModel,
-        NamedPollModel,
-        ConferenceWeekDetailModel,
-        PlenaryMinuteModel,
-      };
-    },
+  await server.start();
+
+  app.get('/health', (req, res) => {
+    res.status(200).send('Okay!');
   });
+
+  app.use(
+    CONFIG.GRAPHQL_PATH,
+    cors<cors.CorsRequest>(),
+    // 50mb is the limit that `startStandaloneServer` uses, but you may configure this to suit your needs
+    bodyParser.json({ limit: '50mb' }),
+    // expressMiddleware accepts the same arguments:
+    // an Apollo Server instance and optional configuration options
+    expressMiddleware(server, {
+      // context: async ({ req }) => ({ token: req.headers.token }),
+      context: async ({ req }) => {
+        return {
+          req,
+          // Models
+          ProcedureModel,
+          UserModel,
+          DeputyModel,
+          NamedPollModel,
+          ConferenceWeekDetailModel,
+          PlenaryMinuteModel,
+        };
+      },
+    }),
+  );
+  await new Promise<void>((resolve) => httpServer.listen({ port: CONFIG.PORT }, resolve));
+  console.log(`🚀 Server ready at http://localhost:${CONFIG.PORT}/`);
+
+  // startStandaloneServer(server, {
+  //   listen: {
+  //     port: CONFIG.PORT,
+  //     path: CONFIG.GRAPHQL_PATH,
+  //   },
+  //   context: async ({ req }) => {
+  //     return {
+  //       req,
+  //       // Models
+  //       ProcedureModel,
+  //       UserModel,
+  //       DeputyModel,
+  //       NamedPollModel,
+  //       ConferenceWeekDetailModel,
+  //       PlenaryMinuteModel,
+  //     };
+  //   },
+  // });
 };
 
 // Async Wrapping Function
