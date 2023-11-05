e6454e2c0ecff462947206cda7e2e1b8c77a83a6
Manuel Ruck <git@manuelruck.de>
2023-11-05 18:44:19 +0100

    fix: 🐛 fix scapacra build
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- services/scrapers/scapacra/src/DataPackage.ts
+++ services/scrapers/scapacra/src/DataPackage.ts
@@ -1,42 +1,42 @@
 /**
  * A data bundle decripes all forms of information it's their metadata.
- * <T>: Datatype of the wrapped data. 
+ * <T>: Datatype of the wrapped data.
  */
-export class DataPackage<D,M> {
-    /**
-     * Meta Data.
-     */
-    public meta: M | null = null;
+export class DataPackage<D, M> {
+  /**
+   * Meta Data.
+   */
+  public meta: M | null = null;
 
-    /**
-     * Raw data.
-     */
-    public data: D | null = null;
+  /**
+   * Raw data.
+   */
+  public data: D | null = null;
 
-    constructor(data: D | null, meta?: M | null){
-        this.setData(data);
-        if(meta){
-            this.setMeta(meta);
-        }
+  constructor(data: D | null, meta?: M | null) {
+    this.setData(data);
+    if (meta) {
+      this.setMeta(meta);
     }
+  }
 
-    public setData(data: D | null) {
-        this.data = data;
-    }
-    public setMeta(meta: M | null) {
-        this.meta = meta;
-    }
+  public setData(data: D | null) {
+    this.data = data;
+  }
+  public setMeta(meta: M | null) {
+    this.meta = meta;
+  }
 
-    public getData(): D | null {
-        return this.data;
-    }
+  public getData(): D | null {
+    return this.data;
+  }
 
-    public getMeta(): M | null {
-        return this.meta;
-    }
+  public getMeta(): M | null {
+    return this.meta;
+  }
 
-    public free(): void {
-        delete this.data;
-        delete this.meta;
-    }
+  public free(): void {
+    this.data = null;
+    this.meta = null;
+  }
 }
--- services/scrapers/scapacra/src/evaluator/DocumentEvaluator.ts
+++ services/scrapers/scapacra/src/evaluator/DocumentEvaluator.ts
@@ -6,100 +6,105 @@ import readline = require('readline');
 export = Documents_Evaluator;
 
 namespace Documents_Evaluator {
+  /**
+   * Evaluates a xPath-Expression to a xml document and returning the matching content as JSON.
+   */
+  export class DocumentEvaluater {
+    private readableStream: NodeJS.ReadableStream;
+
+    private xml2jsOptions = {
+      explicitRoot: false,
+      explicitArray: false,
+      mergeAttrs: true,
+    };
+
     /**
      * Evaluates a xPath-Expression to a xml document and returning the matching content as JSON.
+     *
+     * @param readableStream Stream of the xml document.
      */
-    export class DocumentEvaluater {
-        private readableStream: NodeJS.ReadableStream;
-
-        private xml2jsOptions = {
-            explicitRoot: false,
-            explicitArray: false,
-            mergeAttrs: true
-        };
-
-        /**
-         * Evaluates a xPath-Expression to a xml document and returning the matching content as JSON.
-         * 
-         * @param readableStream Stream of the xml document. 
-         */
-        constructor(readableStream: NodeJS.ReadableStream) {
-            this.readableStream = readableStream;
-        }
-
-        protected xmlDOMErrorCallback(msg: String): void {
-            console.log(`[xmldom error]: ${msg}`);
-        }
-        protected xmlDOMFatalErrorCallback(msg: String): void {
-            console.log(`[xmldom error]: ${msg}`);
-        }
-        protected xmlDOMWarningCallback(msg: String): void {
-            console.log(`[xmldom warning]: ${msg}`);
-        }
-
-
-        public async evaluate(xPathExpression: string): Promise<any[]> {
-            let xml = await this.removeXmlHeader(this.readableStream);
-
-            let parser = new DOMParser({
-                /**
-                 * locator is always need for error position info
-                 */
-                locator: {},
-                /**
-                 * you can override the errorHandler for xml parser
-                 * @link http://www.saxproject.org/apidoc/org/xml/sax/ErrorHandler.html
-                 */
-                errorHandler: {
-                    warning: this.xmlDOMWarningCallback,
-                    error: this.xmlDOMErrorCallback,
-                    fatalError: this.xmlDOMFatalErrorCallback
-                }
-            });
-            let doc = parser.parseFromString(xml);
-
-            let nodes = xpath.select(xPathExpression, doc);
-
-            let elements: any[] = [];
-            for (const node of nodes) {
-                let value = await this.getValueFromSelectedNode(node);
-                elements = elements.concat(value);
-            }
-
-            return elements;
-        }
-
-        protected getValueFromSelectedNode(node: xpath.SelectedValue): Promise<any> {
-            return new Promise((resolve, reject) => {
-                parseString(node, this.xml2jsOptions, (err: any, result: any) => {
-                    if (err == null) {
-                        resolve(result);
-                    } else {
-                        reject(err);
-                    }
-                });
-            });
-        }
-
-        /**
-         * Removes the stylesheet definition and doctype declarion from the xml document to
-         * garentee a proper xPath evaluation.   
-         */
-        private async removeXmlHeader(stream: NodeJS.ReadableStream): Promise<string> {
-            return new Promise((resolve) => {
-                var rl = readline.createInterface(stream);
-                let output: string;
-
-                rl.on('line', (line) => {
-                    let isDeclarationHeader = line.match(/^\<(\?|\!).*$/);
-
-                    if (isDeclarationHeader == null) {
-                        output += line + '\n';
-                    }
-                }).on('close', () => {
-                    resolve(output);
-                });
-            });
-        }
+    constructor(readableStream: NodeJS.ReadableStream) {
+      this.readableStream = readableStream;
     }
+
+    protected xmlDOMErrorCallback(msg: string): void {
+      console.log(`[xmldom error]: ${msg}`);
+    }
+    protected xmlDOMFatalErrorCallback(msg: string): void {
+      console.log(`[xmldom error]: ${msg}`);
+    }
+    protected xmlDOMWarningCallback(msg: string): void {
+      console.log(`[xmldom warning]: ${msg}`);
+    }
+
+    public async evaluate(xPathExpression: string): Promise<any[] | undefined> {
+      const xml = await this.removeXmlHeader(this.readableStream);
+
+      const parser = new DOMParser({
+        /**
+         * locator is always need for error position info
+         */
+        locator: {},
+        /**
+         * you can override the errorHandler for xml parser
+         * @link http://www.saxproject.org/apidoc/org/xml/sax/ErrorHandler.html
+         */
+        errorHandler: {
+          warning: this.xmlDOMWarningCallback,
+          error: this.xmlDOMErrorCallback,
+          fatalError: this.xmlDOMFatalErrorCallback,
+        },
+      });
+      const doc = parser.parseFromString(xml);
+
+      const nodes = xpath.select(xPathExpression, doc) as any[];
+
+      let elements: any[] = [];
+      if (nodes) {
+        for (const node of nodes) {
+          const value = await this.getValueFromSelectedNode(node);
+          elements = elements.concat(value);
+        }
+
+        return elements;
+      }
+    }
+
+    protected getValueFromSelectedNode(node: xpath.SelectedValue): Promise<any> {
+      return new Promise((resolve, reject) => {
+        if (node == null) {
+          return resolve(null);
+        }
+
+        parseString(node, this.xml2jsOptions, (err: any, result: any) => {
+          if (err == null) {
+            resolve(result);
+          } else {
+            reject(err);
+          }
+        });
+      });
+    }
+
+    /**
+     * Removes the stylesheet definition and doctype declarion from the xml document to
+     * garentee a proper xPath evaluation.
+     */
+    private async removeXmlHeader(stream: NodeJS.ReadableStream): Promise<string> {
+      return new Promise((resolve) => {
+        const rl = readline.createInterface(stream);
+        let output: string;
+
+        rl.on('line', (line) => {
+          const isDeclarationHeader = line.match(/^\<(\?|\!).*$/);
+
+          if (isDeclarationHeader == null) {
+            output += line + '\n';
+          }
+        }).on('close', () => {
+          resolve(output);
+        });
+      });
+    }
+  }
 }
