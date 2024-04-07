2718bad09662f2a09853c12b9e41fbfbd8f2065d
Manuel Ruck <git@manuelruck.de>
2024-04-07 14:16:04 +0200

    feat: use api instead of crawling bt website
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- services/cron-jobs/import-plenary-minutes/src/index.ts
+++ services/cron-jobs/import-plenary-minutes/src/index.ts
@@ -1,9 +1,16 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import cheerio from 'cheerio';
 import moment from 'moment';
 import { PlenaryMinuteModel, mongoConnect } from '@democracy-deutschland/bundestagio-common';
 
 import { MetaData, PlenaryMinutesItem } from './types';
+import { Configuration, PlenarprotokolleApi, PlenarprotokollListResponse } from '@democracy-deutschland/bt-dip-sdk';
+import { CONFIG } from './config';
+
+const config = new Configuration({
+  apiKey: `ApiKey ${CONFIG.DIP_API_KEY}`, // Replace #YOUR_API_KEY# with your api key
+});
+const api = new PlenarprotokolleApi(config, undefined, axios);
 
 const AxiosInstance = axios.create();
 
@@ -35,40 +42,50 @@ const getPlenaryMinutes = (plenaryMinutes: cheerio.Cheerio, period: number): Ple
     // Parse Title
     const title = cheerio(elem).find('strong').text().trim();
     const regex = /Plenarprotokoll der (?<meeting>\d{1,3}).*?dem (?<date>.*?)$/gi;
-    const match = regex.exec(title)!.groups as {
-      meeting: string;
-      date: string;
-    };
-    const m = moment(match.date, 'DD MMMM YYYY', 'de');
+    try {
+      const match = regex.exec(title)?.groups as {
+        meeting: string;
+        date: string;
+      };
+      const m = moment(match.date, 'DD MMMM YYYY', 'de');
 
-    // Parse link
-    const xmlLink = cheerio(elem).find('.bt-link-dokument').attr('href');
+      // Parse link
+      const xmlLink = cheerio(elem).find('.bt-link-dokument').attr('href');
 
-    const plenaryMinutesItem: PlenaryMinutesItem = {
-      date: m.toDate(),
-      period,
-      meeting: parseInt(match.meeting),
-      xml: `https://www.bundestag.de${xmlLink}`,
-    };
-    plenaryMinutesItems.push(plenaryMinutesItem);
+      const plenaryMinutesItem: PlenaryMinutesItem = {
+        date: m.toDate(),
+        period,
+        meeting: parseInt(match.meeting),
+        xml: `https://www.bundestag.de${xmlLink}`,
+      };
+      plenaryMinutesItems.push(plenaryMinutesItem);
+    } catch (error) {
+      console.log('error', error, title);
+    }
   });
 
   return plenaryMinutesItems;
 };
 
 const parsePage = async (url: string, period: number) => {
-  return await AxiosInstance.get(url).then((response) => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const meta: cheerio.Cheerio = $('.meta-slider');
-    const plenaryMinutesTable: cheerio.Cheerio = $('.bt-table-data > tbody > tr');
-    const metaData = getMeta(meta);
-    const plenaryMinutes = getPlenaryMinutes(plenaryMinutesTable, period);
-    return {
-      meta: metaData,
-      plenaryMinutes,
-    };
-  });
+  console.log('parsePage', url);
+  return await AxiosInstance.get(url)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const meta: cheerio.Cheerio = $('.meta-slider');
+      const plenaryMinutesTable: cheerio.Cheerio = $('.bt-table-data > tbody > tr');
+      const metaData = getMeta(meta);
+      const plenaryMinutes = getPlenaryMinutes(plenaryMinutesTable, period);
+      return {
+        meta: metaData,
+        plenaryMinutes,
+      };
+    })
+    .catch((error) => {
+      console.error('error', error);
+      throw error;
+    });
 };
 
 const getUrl = ({ offset, id }: { offset: number; id: string }) =>
@@ -79,22 +96,27 @@ const periods = [
   { period: 20, id: '866354-866354' },
 ];
 
-const start = async (period: number) => {
-  const periodId = periods.find((p) => p.period === period)!.id;
-
-  let url: string | false = getUrl({ offset: 0, id: periodId });
-  const data: PlenaryMinutesItem[] = [];
+const start = async () => {
+  const cursor: string | undefined = undefined;
+  const plenarprotokollItems: PlenaryMinutesItem[] = [];
+  let hasNextPage = true;
   do {
-    const { meta, plenaryMinutes } = await parsePage(url, period);
-    data.push(...plenaryMinutes);
-    if (meta.nextOffset < meta.hits) {
-      url = getUrl({ offset: meta.nextOffset, id: periodId });
-    } else {
-      url = false;
+    const { data } = await api.getPlenarprotokollList({ cursor });
+    for (const plenarprotokoll of data.documents) {
+      const regex = /Protokoll der (?<meeting>\d+)\. Sitzung/gi;
+      const match = regex.exec(plenarprotokoll.titel);
+      const meetingNumber = match?.groups?.meeting;
+      plenarprotokollItems.push({
+        date: new Date(plenarprotokoll.datum),
+        period: plenarprotokoll.wahlperiode,
+        meeting: meetingNumber,
+        xml: plenarprotokoll.fundstelle.xml_url,
+      });
     }
-  } while (url);
+    hasNextPage = cursor !== data.cursor;
+  } while (hasNextPage);
   await PlenaryMinuteModel.collection.bulkWrite(
-    data.map((item) => ({
+    plenarprotokollItems.map((item) => ({
       updateOne: {
         filter: { meeting: item.meeting, period: item.period },
         update: {
@@ -104,18 +126,16 @@ const start = async (period: number) => {
       },
     })),
   );
-  console.log(`found for period ${period}: `, data.length);
 };
 
 (async () => {
   console.info('START');
-  console.info('process.env', process.env.DB_URL);
-  if (!process.env.DB_URL) {
+  console.info('process.env', CONFIG.DB_URL);
+  if (!CONFIG.DB_URL) {
     throw new Error('you have to set environment variable: DB_URL');
   }
-  await mongoConnect(process.env.DB_URL);
+  await mongoConnect(CONFIG.DB_URL);
   console.log('PlenaryMinutes', await PlenaryMinuteModel.countDocuments({}));
-  await start(19);
-  await start(20);
+  await start();
   process.exit(0);
 })();
