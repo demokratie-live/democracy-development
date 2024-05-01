bc0553d2dbae414c2d9f418dc06530bcc2ea82e7
Manuel Ruck <git@manuelruck.de>
2024-05-01 18:45:42 +0200

    chore: add garden.io
    to make it much more easy to start with local development in an kubernetes cluster environment which is a large shift left and improves developer expirience
    
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- services/qr-code-handler/src/index.ts
+++ services/qr-code-handler/src/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
-import mongoConnect from "./mongoose";
-import { EntryModel } from "./models";
+import express from 'express';
+import mongoConnect from './mongoose';
+import { EntryModel } from './models';
 
 const app = express();
 const port = 3000;
 
-app.get("/:code", (req, _res, next) => {
-  var code = req.params.code?.toLowerCase();
+app.get('/:code', (req, _res, next) => {
+  const code = req.params.code?.toLowerCase();
   if (code && code.length > 0 && code.match(/^[0-9a-z]+$/)) {
     console.log(`CODE: "${code}" redirected`);
     EntryModel.create({ code });
@@ -17,8 +17,8 @@ app.get("/:code", (req, _res, next) => {
   next();
 });
 
-app.get("*", (_req, res) => {
-  res.status(301).redirect("https://www.democracy-deutschland.de");
+app.get('*', (_req, res) => {
+  res.status(301).redirect('https://www.democracy-deutschland.de');
 });
 
 app.listen(port, async () => {
