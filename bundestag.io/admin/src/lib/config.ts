a5b112674f3deab43a3bd6af2fef9d67353d96ad
Manuel Ruck <git@manuelruck.de>
2024-05-09 15:26:24 +0200

    feat: enable auto select for votes-ai
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- bundestag.io/admin/src/lib/config.ts
+++ bundestag.io/admin/src/lib/config.ts
@@ -1,11 +1,13 @@
 let errors: string[] = [];
+const AI_SIMULATION = process.env.AI_SIMULATION === 'true';
+
 if (!process.env.BUNDESTAGIO_SERVER_URL) {
   errors.push('Please define the BUNDESTAGIO_SERVER_URL environment variable inside .env.local');
 }
-if (!process.env.AI_SERVER_URL) {
+if (!AI_SIMULATION && !process.env.AI_SERVER_URL) {
   errors.push('Please define the AI_SERVER_URL environment variable inside .env.local');
 }
-if (!process.env.AI_ACCESS_TOKEN) {
+if (!AI_SIMULATION && !process.env.AI_ACCESS_TOKEN) {
   errors.push('Please define the AI_ACCESS_TOKEN environment variable inside .env.local');
 }
 
@@ -15,6 +17,7 @@ if (errors.length > 0) {
 
 export const config = {
   BUNDESTAGIO_SERVER_URL: process.env.BUNDESTAGIO_SERVER_URL!,
-  AI_SERVER_URL: process.env.AI_SERVER_URL!,
-  AI_ACCESS_TOKEN: process.env.AI_ACCESS_TOKEN!,
+  AI_SERVER_URL: process.env.AI_SERVER_URL,
+  AI_ACCESS_TOKEN: process.env.AI_ACCESS_TOKEN,
+  AI_SIMULATION: AI_SIMULATION,
 };
