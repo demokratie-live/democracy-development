86ba94be996fb070df253cbd24701f76e0977633
Manuel Ruck <git@manuelruck.de>
2024-06-10 20:18:29 +0200

    test: 🧪 add integration tests for verify device
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- src/graphql/resolvers/Device.integ.ts
+++ src/graphql/resolvers/Device.integ.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import crypto from 'crypto';
 
 const GRAPHQL_API_URL = process.env.GRAPHQL_API_URL || 'http://localhost:3000';
 
@@ -104,9 +105,9 @@ describe('Device GraphQL API', () => {
           },
         },
       );
-  
+
       const { data } = response.data;
-  
+
       expect(data).toBeDefined();
       expect(data.notificationSettings).toBeDefined();
       expect(data.notificationSettings.enabled).toBeTruthy();
@@ -114,6 +115,146 @@ describe('Device GraphQL API', () => {
       expect(data.notificationSettings.voteConferenceWeekPushs).toBeTruthy();
       expect(data.notificationSettings.voteTOP100Pushs).toBeTruthy();
       expect(data.notificationSettings.outcomePushs).toBeTruthy();
+    });
+  });
+
+  describe('verify device', () => {
+    const SOME_DEVICE_HASH = Math.random().toString(36).substring(7);
+    const RANDOM_PHONE_NUMBER = `+49${Math.floor(Math.random() * 1000000000)}`;
+    it('request verification code via sms', async () => {
+      const response = await axios.post(
+        GRAPHQL_API_URL,
+        {
+          query: `
+            mutation RequestSmsCode($newPhone: String!, $oldPhoneHash: String) {
+              requestCode(newPhone: $newPhone, oldPhoneHash: $oldPhoneHash) {
+                reason
+                allowNewUser
+                succeeded
+                resendTime
+                expireTime
+              }
+            }
+          `,
+          variables: {
+            newPhone: RANDOM_PHONE_NUMBER,
+            oldPhoneHash: '',
+          },
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'x-device-hash': SOME_DEVICE_HASH,
+          },
+        },
+      );
+
+      const { data } = response.data;
+
+      expect(data).toBeDefined();
+      expect(data.requestCode).toBeDefined();
+      expect(data.requestCode.succeeded).toBeTruthy();
+    });
+
+    it('request fast second verification code via sms', async () => {
+      const response = await axios.post(
+        GRAPHQL_API_URL,
+        {
+          query: `
+            mutation RequestSmsCode($newPhone: String!, $oldPhoneHash: String) {
+              requestCode(newPhone: $newPhone, oldPhoneHash: $oldPhoneHash) {
+                reason
+                allowNewUser
+                succeeded
+                resendTime
+                expireTime
+              }
+            }
+          `,
+          variables: {
+            newPhone: RANDOM_PHONE_NUMBER,
+            oldPhoneHash: '',
+          },
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'x-device-hash': SOME_DEVICE_HASH,
+          },
+        },
+      );
+
+      const { data } = response.data;
+
+      expect(data).toBeDefined();
+      expect(data.requestCode).toBeDefined();
+      expect(data.requestCode.succeeded).toBeFalsy();
+    });
+
+    it('try to verify phone number with wrong code', async () => {
+      const newPhoneHash = crypto.createHash('sha256').update(RANDOM_PHONE_NUMBER).digest('hex');
+      const response = await axios.post(
+        GRAPHQL_API_URL,
+        {
+          query: `
+            mutation RequestVerification($code: String!, $newPhoneHash: String!, $newUser: Boolean) {
+              requestVerification(code: $code, newPhoneHash: $newPhoneHash, newUser: $newUser) {
+                reason
+                succeeded
+              }
+            }
+          `,
+          variables: {
+            code: '123456',
+            newPhoneHash,
+          },
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'x-device-hash': SOME_DEVICE_HASH,
+          },
+        },
+      );
+
+      const { data } = response.data;
+
+      expect(data).toBeDefined();
+      expect(data.requestVerification).toBeDefined();
+      expect(data.requestVerification.succeeded).toBeFalsy();
+    });
+
+    it('verify phone number', async () => {
+      const newPhoneHash = crypto.createHash('sha256').update(RANDOM_PHONE_NUMBER).digest('hex');
+      const response = await axios.post(
+        GRAPHQL_API_URL,
+        {
+          query: `
+            mutation RequestVerification($code: String!, $newPhoneHash: String!, $newUser: Boolean) {
+              requestVerification(code: $code, newPhoneHash: $newPhoneHash, newUser: $newUser) {
+                reason
+                succeeded
+              }
+            }
+          `,
+          variables: {
+            code: '000000',
+            newPhoneHash,
+          },
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'x-device-hash': SOME_DEVICE_HASH,
+          },
+        },
+      );
+
+      const { data } = response.data;
+
+      expect(data).toBeDefined();
+      expect(data.requestVerification).toBeDefined();
+      expect(data.requestVerification.succeeded).toBeTruthy();
     });
   });
 });
