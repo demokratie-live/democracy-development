8beb75f2fd38ff079298accdd99c54ffe8fa2459
Manuel Ruck <git@manuelruck.de>
2024-08-11 20:25:55 +0200

    feat: add indicator to the button of non-named-vote-results
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- bundestag.io/admin/src/components/Procedures/Beschlusstext.tsx
+++ bundestag.io/admin/src/components/Procedures/Beschlusstext.tsx
@@ -1,5 +1,5 @@
 import { log } from '@/lib/logger';
-import { Button } from 'antd';
+import { Button, Spin } from 'antd';
 import FormItem from 'antd/es/form/FormItem';
 import TextArea from 'antd/es/input/TextArea';
 import axios from 'axios';
@@ -13,20 +13,24 @@ type Props = {
 
 export const Beschlusstext: React.FC<Props> = ({ pdfUrl, title, drucksachen }) => {
   const [aiText, setAiText] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
   const onGetViaAi = async () => {
     log.debug(`request non-named-votes-ai: ${pdfUrl}, ${title}, ${drucksachen}`);
+    setLoading(true);
     const response = await axios.get('/api/non-named-votes-ai/get', {
       params: { pdfUrl, title, drucksachen },
     });
     setAiText(response.data.text);
+    setLoading(false);
   };
 
   return (
     <FormItem label="Beschlusstext" name="decisionText" rules={[{ required: true, message: 'Beschlusstext fehlt!' }]}>
       <TextArea placeholder="Beschlusstext" rows={3} />
       {!aiText && (
-        <Button type="dashed" onClick={onGetViaAi}>
-          get via AI
+        <Button type="dashed" onClick={onGetViaAi} disabled={loading}>
+          {!loading ? 'get via AI' : <Spin />}
         </Button>
       )}
       {aiText && <div>{aiText}</div>}
