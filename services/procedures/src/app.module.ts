a229563b21006ec3e24c931a7ab1a48ea9963e5f
Manuel Ruck <git@manuelruck.de>
2024-09-17 21:46:23 +0200

    feat(procedures): 🚀 add procedures findAll endpoint
    GET /health
    
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- services/procedures/src/app.module.ts
+++ services/procedures/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProceduresModule } from './procedures/procedures.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ProceduresModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/bundestagio'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
