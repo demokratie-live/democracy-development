0dba695bb5388d848da020e2dfbb19363c142e71
Manuel Ruck <git@manuelruck.de>
2024-09-22 07:26:52 +0200

    test(e2e): 🔂 fix health e2e test
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- services/procedures/test/app.e2e-spec.ts
+++ services/procedures/test/app.e2e-spec.ts
@@ -15,10 +15,14 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('/health (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/health')
       .expect(200)
-      .expect('Hello World!');
+      .expect('ok!');
   });
 });
