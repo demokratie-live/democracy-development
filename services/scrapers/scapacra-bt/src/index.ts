7ad900671c1b06ded0f8feb35c7d602f2efce1a7
Manuel Ruck <git@manuelruck.de>
2025-02-22 16:02:00 +0100

    fix: tests
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- services/scrapers/scapacra-bt/src/index.ts
+++ services/scrapers/scapacra-bt/src/index.ts
@@ -1,19 +1,6 @@
-/*import { IProtocolScraperConfigurationOptions } from './unused/config/ProtocolScraperConfiguration';
-import { ProtocolSpeechScraperConfiguration } from './unused/config/ProtocolSpeechScraperConfiguration';
-import { ProtocolVotingScraperConfiguration } from './unused/config/ProtocolVotingScraperConfiguration';
-import { ProposedDecisionScraperConfiguration } from './unused/config/ProposedDecisionScraperConfiguration';*/
 import { DeputyProfileScraper } from './DeputyProfile/DeputyProfileScraper';
 import { NamedPollScraper } from './NamedPoll/NamedPollScraper';
 import { NamedPollDeputyScraper } from './NamedPollDeputy/NamedPollDeputyScraper';
 import { ConferenceWeekDetailScraper } from './ConferenceWeekDetail/ConferenceWeekDetailScraper';
 
-export {
-    /*IProtocolScraperConfigurationOptions,
-    ProtocolSpeechScraperConfiguration,
-    ProtocolVotingScraperConfiguration,
-    ProposedDecisionScraperConfiguration,*/
-    DeputyProfileScraper,
-    NamedPollScraper,
-    NamedPollDeputyScraper,
-    ConferenceWeekDetailScraper
-}
+export { DeputyProfileScraper, NamedPollScraper, NamedPollDeputyScraper, ConferenceWeekDetailScraper };
