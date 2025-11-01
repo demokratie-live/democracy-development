be27416c916ab0186f6e3fffd7b03d9124367b6d
Manuel Ruck <git@manuelruck.de>
2025-11-01 10:24:33 +0100

    fix(types): make Express Request an interface extending ExpressReqContext
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,5 +1,14 @@
-import { ExpressReqContext } from './graphqlContext';
+import { User, Phone, Device } from '@democracy-deutschland/democracy-common';
+import { Request } from 'express';
 
-declare namespace Express {
-  export type Request = ExpressReqContext
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User | null;
+      phone?: Phone | null;
+      device?: Device | null;
+      version?: string;
+      applicationId?: string;
+    }
+  }
 }
